bf4cba7124cdc80950397ff87821e881298ec1a9
Jo Gayed <joseph.gayed91@gmail.com>
2024-03-04 02:30:15 +0200

    initial commit with empty project

---
+++ Coptix-iOS/Coptix_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coptix_iOSApp.swift
+//  Coptix-iOS
+//
+//  Created by Jo on 04/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Coptix_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
